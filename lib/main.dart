13f06b3455ac833b5091856e2ad178f4b396ce11
dawa-pic <dawan7548@gmail.com>
2025-06-13 19:18:51 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Runner Game Placeholder')),
+      body: Center(child: Text('Runner Game is working!')),
+    ),
+  ));
+}
